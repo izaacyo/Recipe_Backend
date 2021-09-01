5a7b9bf10044ba4f8096df35f13f7eb2b04af2ac
izaacyo <mariusppetrut@gmail.com>
2021-09-02 01:33:40 +0300

    db migrated

---
+++ migrations/Version20210901222024.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210901222024 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE recipes (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, time VARCHAR(20) NOT NULL, difficulty VARCHAR(30) NOT NULL, portions VARCHAR(30) NOT NULL, ingredients CLOB NOT NULL --(DC2Type:array)
+        , instructions CLOB NOT NULL --(DC2Type:array)
+        )');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE recipes');
+    }
+}
