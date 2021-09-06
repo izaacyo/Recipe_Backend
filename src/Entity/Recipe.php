677f258cad4501e0b4dba2eac1147a2ba7d0ac87
izaacyo <mariusppetrut@gmail.com>
2021-09-06 13:55:02 +0300

    migration done

---
+++ src/Entity/Recipe.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RecipeRepository::class)
+ */
+class Recipe
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $difficulty;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $portions;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private $ingredients = [];
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private $instructions = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTime(): ?string
+    {
+        return $this->time;
+    }
+
+    public function setTime(string $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?string
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(string $difficulty): self
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function getPortions(): ?string
+    {
+        return $this->portions;
+    }
+
+    public function setPortions(string $portions): self
+    {
+        $this->portions = $portions;
+
+        return $this;
+    }
+
+    public function getIngredients(): ?array
+    {
+        return $this->ingredients;
+    }
+
+    public function setIngredients(array $ingredients): self
+    {
+        $this->ingredients = $ingredients;
+
+        return $this;
+    }
+
+    public function getInstructions(): ?array
+    {
+        return $this->instructions;
+    }
+
+    public function setInstructions(array $instructions): self
+    {
+        $this->instructions = $instructions;
+
+        return $this;
+    }
+}
